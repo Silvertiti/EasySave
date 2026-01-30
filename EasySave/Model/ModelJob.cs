dec32e3651494d04bcd9e0eeaeb451ceddafdaed
Silvertiti <metheotim2@gmail.com>
2026-01-30 14:37:11 +0100

    Debut

---
+++ EasySave/Model/ModelJob.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EasySave.Models
+{
+
+    public class ModelJob
+    {
+        public string Name { get; set; }
+        public string Source { get; set; }
+        public string Target { get; set; }
+
+    }
+}
+
